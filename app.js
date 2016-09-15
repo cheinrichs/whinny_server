4948559b0b8e9124ef2402e3e09cbd4484b9e078
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2016-09-15 16:19:58 -0600

    added cors

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var cors = require('cors');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var messages = require('./routes/messages');
 
 var app = express();
+
+app.use(cors());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
