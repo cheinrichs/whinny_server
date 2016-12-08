8db9dc9a1c56b098068ffc362acd25f00a2a6c30
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2016-12-07 18:10:14 -0700

    updated seeds

--- seeds/06_broadcast_messages.js
+++ seeds/06_broadcast_messages.js
@@ -8,7 +8,7 @@ exports.seed = function(knex, Promise) {
         knex('broadcast_messages').insert({
           to_broadcast: 1,
           from_user: 1,
-          broadcast_photo_url: 'http://vignette1.wikia.nocookie.net/disney/images/4/4d/Winniethepooh-disneyscreencaps.com-4335.jpg/revision/latest?cb=20110508025700',
+          broadcast_photo_url: 'https://s3.amazonaws.com/whinnyphotos/broadcast_message_photos/broadcast_message_photo_1.jpg',
           broadcast_title: 'Woozles. I repeat. Woozles',
           broadcast_message: 'Wooozles are currently affecting barns all across the front range',
           link_text: 'Whinny the Pooh Song',
@@ -17,7 +17,7 @@ exports.seed = function(knex, Promise) {
         knex('broadcast_messages').insert({
           to_broadcast: 2,
           from_user: 1,
-          broadcast_photo_url: 'http://cdn.thefiscaltimes.com/sites/default/files/KentuckyDerbyRace.jpeg',
+          broadcast_photo_url: 'https://s3.amazonaws.com/whinnyphotos/broadcast_message_photos/broadcast_message_photo_2.jpg',
           broadcast_title: 'Race Results 12/19/16',
           broadcast_message: 'This horse was faster than that horse. If you lost money please don\'t blame us',
           link_text: 'Duck Blur',
@@ -26,7 +26,7 @@ exports.seed = function(knex, Promise) {
         knex('broadcast_messages').insert({
           to_broadcast: 3,
           from_user: 2,
-          broadcast_photo_url: 'http://www.boydandsilvamartin.com/wp-content/uploads/2009/03/Palm-Beach-Dressage-Derby-252.jpg',
+          broadcast_photo_url: 'https://s3.amazonaws.com/whinnyphotos/broadcast_message_photos/broadcast_message_photo_3.jpg',
           broadcast_title: 'Show Attire Update',
           broadcast_message: 'Please note: Puting a tutu on your horse will not help it\'s pirouettes',
           link_text: 'Dancing queen',
@@ -35,11 +35,29 @@ exports.seed = function(knex, Promise) {
         knex('broadcast_messages').insert({
           to_broadcast: 4,
           from_user: 2,
-          broadcast_photo_url: 'http://vignette1.wikia.nocookie.net/disney/images/4/4d/Winniethepooh-disneyscreencaps.com-4335.jpg/revision/latest?cb=20110508025700',
-          broadcast_title: 'Lost Dog',
-          broadcast_message: 'We have a lost long hair dachshund. He is demanding roast chicken at the front office.',
-          link_text: 'Contact',
-          link_url: 'http://rejectionline.com/'
+          broadcast_photo_url: 'https://s3.amazonaws.com/whinnyphotos/broadcast_message_photos/broadcast_message_photo_4.jpg',
+          broadcast_title: '',
+          broadcast_message: 'I think we can all relate',
+          link_text: '',
+          link_url: ''
+        }),
+        knex('broadcast_messages').insert({
+          to_broadcast: 4,
+          from_user: 2,
+          broadcast_photo_url: 'https://s3.amazonaws.com/whinnyphotos/broadcast_message_photos/broadcast_message_photo_5.jpg',
+          broadcast_title: '',
+          broadcast_message: 'So true',
+          link_text: '',
+          link_url: ''
+        }),
+        knex('broadcast_messages').insert({
+          to_broadcast: 4,
+          from_user: 2,
+          broadcast_photo_url: 'https://s3.amazonaws.com/whinnyphotos/broadcast_message_photos/broadcast_message_photo_6.jpg',
+          broadcast_title: '',
+          broadcast_message: 'I wouldn\'t question him',
+          link_text: '',
+          link_url: ''
         })
       ]);
     });
