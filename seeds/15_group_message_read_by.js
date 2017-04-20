64ced9c268a972872f39beaad8334ca06e14463f
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2017-04-20 13:16:49 -0600

    seed fixes

--- seeds/15_group_message_read_by.js
+++ seeds/15_group_message_read_by.js
@@ -11,14 +11,20 @@ exports.seed = function(knex, Promise) {
         createGroupReadByRecord('2', '1', 'Testing - GFF', false),
         createGroupReadByRecord('4', '1', 'Testing - Whinny Staff', false),
         createGroupReadByRecord('5', '1', 'Testing - Paragon Equestrian Centre', false),
+        createGroupReadyByRecordImage('4', '1', 'https://thewondrous.com/wp-content/uploads/2015/05/horse-funny.jpg', false),
 
         createGroupReadByRecord('2', '2', 'Testing - GFF', true),
         createGroupReadByRecord('4', '2', 'Testing - Whinny Staff', true),
         createGroupReadByRecord('5', '2', 'Testing - Paragon Equestrian Centre', true),
+        createGroupReadyByRecordImage('4', '2', 'https://thewondrous.com/wp-content/uploads/2015/05/horse-funny.jpg', false),
 
         createGroupReadByRecord('2', '3', 'Testing - GFF', false),
         createGroupReadByRecord('4', '3', 'Testing - Whinny Staff', false),
         createGroupReadByRecord('5', '3', 'Testing - Paragon Equestrian Centre', false),
+        createGroupReadyByRecordImage('4', '2', 'https://thewondrous.com/wp-content/uploads/2015/05/horse-funny.jpg', false),
+
+
+
 
       ]);
     });
@@ -31,6 +37,19 @@ exports.seed = function(knex, Promise) {
         time_read: null
       };
       return knex('group_messages').where('group_message_content', content).first().then(function (message) {
+        result.group_message_id = message.group_message_id;
+        return knex('group_message_read_by').insert(result);
+      });
+    }
+
+    function createGroupReadyByRecordImage(group_id, to_user_id, link, read) {
+      var result = {
+        group_id: group_id,
+        to_user_id: to_user_id,
+        read: read,
+        time_read: null
+      };
+      return knex('group_messages').where('image_src', link).first().then(function (message) {
         result.group_message_id = message.group_message_id;
         return knex('group_message_read_by').insert(result);
       });
