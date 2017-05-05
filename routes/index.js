64527a75f3ac906ff63ce1a4bcb07dc72f7e732b
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2017-05-05 09:16:39 -0600

    debugging update email and password

--- routes/index.js
+++ routes/index.js
@@ -355,48 +355,49 @@ router.post('/updateEmailAndPassword', function (req, res, next) {
 
   console.log(req.body.email);
 
-  var userPassword;
-  bcrypt.hash(req.body.password, SALT_ROUNDS, function(err, hash) {
-    console.log("hash", hash);
-    userPassword = hash;
-  });
-
   var newEmailConfirmationCode = generateEmailConfirmationCode();
   console.log(newEmailConfirmationCode);
 
   var confirmationLink = "https://whinny-staging.herokuapp.com/confirmEmail/" + req.body.user_id + "/" + newEmailConfirmationCode;
 
-  knex('users').where('user_id', req.body.user_id).update({email: req.body.email, password: userPassword, email_confirmation_code: newEmailConfirmationCode}).then(function () {
-    sp.transmissions.send({
-      recipients: [
-        {
-          "address": {
-            "email": req.body.email,
-            "name": 'Whinny'
+  var userPassword;
+  bcrypt.hash(req.body.password, SALT_ROUNDS, function(err, hash) {
+    console.log("hash", hash);
+    userPassword = hash;
+    knex('users').where('user_id', req.body.user_id).update({email: req.body.email, password: userPassword, email_confirmation_code: newEmailConfirmationCode}).then(function () {
+      sp.transmissions.send({
+        recipients: [
+          {
+            "address": {
+              "email": req.body.email,
+              "name": 'Whinny'
+            }
           }
-        }
-      ],
-      content: {
-        from: {
-          "name": "Whinny Server",
-          "email": "postmaster@whinny.com"
-        },
-        subject: 'Whinny: Confirm your email' ,
-        html: "Click this link to confirm your email address: <a href=" + confirmationLink + ">" + confirmationLink + "</a>",
+        ],
+        content: {
+          from: {
+            "name": "Whinny Server",
+            "email": "postmaster@whinny.com"
+          },
+          subject: 'Whinny: Confirm your email' ,
+          html: "Click this link to confirm your email address: <a href=" + confirmationLink + ">" + confirmationLink + "</a>",
 
-      }
-    }, function (err, apiResponse) {
-      console.log(err);
-      console.log(apiResponse);
-      if(err){
-        console.log("error in sp transmission");
-        res.json(err);
-      } else {
-        console.log("confirmation sent");
-        res.json({ confirmationEmail: "sent"})
-      }
-    });
-  })
+        }
+      }, function (err, apiResponse) {
+        console.log(err);
+        console.log(apiResponse);
+        if(err){
+          console.log("error in sp transmission");
+          res.json(err);
+        } else {
+          console.log("confirmation sent");
+          res.json({ confirmationEmail: "sent"})
+        }
+      });
+    })
+  });
+
+
 })
 
 router.get('/confirmEmail/:user_id/:confirmation_code', function (req, res, next) {
