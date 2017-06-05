4f1b0b03b63625552d4741b0bb3e590f69488f8a
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2017-06-05 15:22:48 -0600

    removed barn alert in refactor

--- routes/index.js
+++ routes/index.js
@@ -1847,55 +1847,7 @@ router.post('/logOut', function (req, res, next) {
   })
 })
 
-
-// ******** Barn Alert ************* //
-
-router.get('/getEdccTwitterMessages', function (req, res, next) {
-  var params = {screen_name: 'EquineDiseaseCC', count: 5};
-  twitterClient.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) {
-      console.log(tweets);
-      res.json({twitterMessages: tweets });
-    } else {
-      res.json({error})
-    }
-  });
-})
-
-ontime({
-  cycle: ['2:30:00', '3:00:00', '12:00:00']
-}, function(ot){
-  //look through edcc tweets, check against tweets in the database
-  //add them to the database
-  //keep new tweets and add them to an object organized by state?
-  //var new barnAlerts = {
-    // CO: [
-      'Measles'
-    //]
-  //}
-  //parse for locations where we have subscribed warnings,
-  //send texts to all subscribed users
-  sendBarnAlert('3035892321', '2:30pm, 3pm or 12pm. Test message from barn alert. https://t.co/50EBYeawMH');
-  ot.done();
-  return;
-})
-
-function sendBarnAlert(to_phone, content) {
-  textClient.sms.messages.create({
-    to: to_phone,
-    from: '+17204087635',
-    body: content
-  }, function (error, message) {
-    if(!error){
-      console.log("Success! The SID for this SMS message is: ", message.sid);
-    } else {
-      console.log("There was an error with twilio mms message");
-    }
-  })
-}
-
-
-// ******** Whinny Helper Functions ************* //
+// ******** Helper Functions ************* //
 
 Array.prototype.unique = function() {
     var o = {};
